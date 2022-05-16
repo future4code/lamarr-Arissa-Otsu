ae0f86057c5f921c98ccd2544a46b064e4b8c88e
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2022-05-15 21:15:07 -0300

    Resolução do projeto de fixação: BlackJack 3 - Após entrega e correção

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -16,6 +16,8 @@ let cartasDoComputador = []
 let pontuacaoDoUsuario = 0
 let pontuacaoDoComputador = 0
 let textoDoUsuario = ""
+let textoDoComputador = ""
+
 function adicionarCarta (carta) {
    let cartaSorteada = comprarCarta()
    carta.push(cartaSorteada)
@@ -28,22 +30,78 @@ if(confirm("Boas vindas ao jogo de Blackjack!" + "\n" +"Quer iniciar uma nova ro
    adicionarCarta(cartasDoComputador)
    adicionarCarta(cartasDoComputador)
 
-   console.log(cartasDoUsuario)
-   if (cartasDoUsuario[0].valor === 11 && cartasDoUsuario[1].valor === 11){
+   while ((cartasDoUsuario[0].valor === 11 && cartasDoUsuario[1].valor === 11) || (cartasDoComputador[0].valor === 11 && cartasDoComputador[1].valor === 11)) {
       cartasDoUsuario = []
-   } else if (cartasDoComputador[0].valor === 11 && cartasDoComputador[1].valor === 11){
       cartasDoComputador = []
-   } else {
-/*       for (i=0; i<cartasDoUsuario.length;i++) {
+      adicionarCarta(cartasDoUsuario)
+      adicionarCarta(cartasDoUsuario)
+      adicionarCarta(cartasDoComputador)
+      adicionarCarta(cartasDoComputador)
+   } 
+   
+   textoDoUsuario = cartasDoUsuario[0].texto + cartasDoUsuario[1].texto
+   pontuacaoDoUsuario = cartasDoUsuario[0].valor + cartasDoUsuario[1].valor
+   textoDoComputador = cartasDoComputador[0].texto + cartasDoComputador[1].texto
+   pontuacaoDoComputador = cartasDoComputador[0].valor + cartasDoComputador[1].valor
+
+   while ((pontuacaoDoUsuario < 21) && (confirm(`Suas cartas são ${textoDoUsuario}. A carta revelada do computador é ${cartasDoComputador[0].texto}` + "\n" +"Deseja comprar mais uma carta?")) ) {
+      adicionarCarta(cartasDoUsuario)
+      textoDoUsuario = ""
+      pontuacaoDoUsuario = 0
+      textoDoComputador = ""
+      pontuacaoDoComputador = 0  
+      for (i=0; i<cartasDoUsuario.length; i++) {
          pontuacaoDoUsuario += cartasDoUsuario[i].valor
          textoDoUsuario += cartasDoUsuario[i].texto
-      console.log(pontuacaoDoUsuario) */
-         if(confirm(`Suas cartas são ${cartasDoUsuario[0].texto} e ${cartasDoUsuario[1].texto}. A carta revelelada do computador é ${cartasDoComputador[0].texto}` + "\n" +"Deseja comprar mais uma carta?")) {
-            adicionarCarta(cartasDoUsuario)
+         pontuacaoDoComputador = cartasDoComputador[0].valor + cartasDoComputador[1].valor
+         textoDoComputador = cartasDoComputador[0].texto + cartasDoComputador[1].texto
+      } 
+   } 
+   
+   if (pontuacaoDoUsuario <= 21){
+      while ((pontuacaoDoComputador < pontuacaoDoUsuario) && (pontuacaoDoComputador <=21)) { 
+         adicionarCarta(cartasDoComputador)
+         textoDoComputador = ""
+         pontuacaoDoComputador = 0  
+         for(i=0; i<cartasDoComputador.length; i++){
+            pontuacaoDoComputador += cartasDoComputador[i].valor
+            textoDoComputador += cartasDoComputador[i].texto    
          }
       }
+   }
+
+   if (pontuacaoDoUsuario > pontuacaoDoComputador && pontuacaoDoUsuario <=21) {
+      alert (`Usuario - Cartas: ${textoDoUsuario} - Pontuação: ${pontuacaoDoUsuario}` + 
+            "\n" + 
+            `Computador - Cartas: ${textoDoComputador} - Pontuação: ${pontuacaoDoComputador}` + 
+            "\n" + 
+            "O usuário ganhou!")
+   } else if (pontuacaoDoComputador > pontuacaoDoUsuario && pontuacaoDoComputador <=21) {
+      alert (`Usuario - Cartas: ${textoDoUsuario} - Pontuação: ${pontuacaoDoUsuario}` + 
+            "\n" + 
+            `Computador - Cartas: ${textoDoComputador} - Pontuação: ${pontuacaoDoComputador}` + 
+            "\n" + 
+            "O computador ganhou!")
+   } else if (pontuacaoDoComputador > 21 && pontuacaoDoUsuario <= 21) {
+      alert (`Usuario - Cartas: ${textoDoUsuario} - Pontuação: ${pontuacaoDoUsuario}` + 
+            "\n" + 
+            `Computador - Cartas: ${textoDoComputador} - Pontuação: ${pontuacaoDoComputador}` + 
+            "\n" + 
+            "O usuário ganhou!")
+   } else if (pontuacaoDoUsuario > 21 && pontuacaoDoComputador <= 21){
+      alert (`Usuario - Cartas: ${textoDoUsuario} - Pontuação: ${pontuacaoDoUsuario}` + 
+            "\n" + 
+            `Computador - Cartas: ${textoDoComputador} - Pontuação: ${pontuacaoDoComputador}` + 
+            "\n" + 
+            "O computador ganhou!")
+   } else {
+      alert (`Usuario - Cartas: ${textoDoUsuario} - Pontuação: ${pontuacaoDoUsuario}` + 
+            "\n" + 
+            `Computador - Cartas: ${textoDoComputador} - Pontuação: ${pontuacaoDoComputador}` + 
+            "\n" + 
+            "Empate!")
+   }
+
+} else {
+   alert ("O jogo acabou.")
 }
-
-
-
-
