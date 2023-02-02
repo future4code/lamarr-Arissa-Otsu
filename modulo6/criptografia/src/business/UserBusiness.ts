440c8f677f2cad14013028df4270cd8e1ce445ca
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2023-02-02 18:01:23 -0300

    Resolução dos exercícios de criptografia

---
+++ modulo6/criptografia/src/business/UserBusiness.ts
@@ -0,0 +1,138 @@
+import { UserDatabase } from "../data/UserDatabase";
+import { CustomError, InvalidEmail, InvalidName, InvalidPassword, Unauthorized, UserNotFound } from "../error/customError";
+import {
+  UserInputDTO,
+  user,
+  EditUserInputDTO,
+  EditUserInput,
+  LoginInputDTO,
+} from "../model/user";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
+import { TokenGenerator } from "../services/TokenGenerator";
+
+const idGenerator = new IdGenerator()
+const tokenGenerator = new TokenGenerator()
+const userDatabase = new UserDatabase();
+const hashManager = new HashManager();
+
+export class UserBusiness {
+  public createUser = async (input: UserInputDTO): Promise<string> => {
+    try {
+      const { name, nickname, email, password } = input;
+
+      if (!name || !nickname || !email || !password) {
+        throw new CustomError(
+          400,
+          'Preencha os campos "name","nickname", "email" e "password"'
+        );
+      }
+
+      if (name.length < 4) {
+        throw new InvalidName();
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      const id: string = idGenerator.generateId()
+
+      const hashPassword: string = await hashManager.generateHash(password)
+
+      const user: user = {
+        id,
+        name,
+        nickname,
+        email,
+        password: hashPassword,
+      };
+
+      await userDatabase.insertUser(user);
+      const token = tokenGenerator.generateToken(id)
+
+      return token
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public login = async (input: LoginInputDTO): Promise<string> => {
+    try {
+      const { email, password } = input;
+    
+      if (!email || !password) {
+        throw new CustomError(
+          400,
+          'Preencha os campos"email" e "password"'
+        );
+      }
+
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
+
+      const user = await userDatabase.findUser(email);
+
+      if (!user) {
+        throw new UserNotFound()
+      }
+
+      const compareResult: boolean = await hashManager.compareHash(password, user.password)
+
+      if(!compareResult){ 
+        throw new InvalidPassword()
+      }
+
+      const token = tokenGenerator.generateToken(user.id)
+
+      return token
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public editUser = async (input: EditUserInputDTO) => {
+    try {
+      const { name, nickname, id, token } = input;
+
+      if (!name || !nickname || !id || !token) {
+        throw new CustomError(
+          400,
+          'Preencha os campos "id", "name", "nickname" e "token"'
+        );
+      }
+
+      const data = tokenGenerator.tokenData(token)
+
+      if(!data.id) {
+        throw new Unauthorized()
+      }
+
+      if (name.length < 4) {
+        throw new InvalidName();
+      }
+
+      const editUserInput: EditUserInput = {
+        id,
+        name,
+        nickname,
+      };
+
+      const userDatabase = new UserDatabase();
+      await userDatabase.editUser(editUserInput);
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public getUserById = async(token:string)=>{
+    try {
+      const userDatabase = new UserDatabase()
+      const user = await userDatabase.getUserById(token);
+      return user
+    } catch (err:any) {
+      throw new Error(err.message);
+    }
+}
+}
---
+++ modulo6/criptografia/src/data/UserDatabase.ts
@@ -0,0 +1,56 @@
+import { CustomError } from "../error/customError";
+import { EditUserInput, user } from "../model/user";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  public findUser = async (email: string) => {
+    try {
+  
+      const result = await UserDatabase.connection
+        .select()
+        .where({email})
+        .from("Auth_users")
+
+      
+      return result[0];
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public insertUser = async (user: user) => {
+    try {
+      await UserDatabase.connection
+        .insert({
+          id: user.id,
+          name: user.name,
+          nickname: user.nickname,
+          email: user.email,
+          password: user.password,
+        })
+        .into("Auth_users");
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public editUser = async (user: EditUserInput) => {
+    try {
+      await UserDatabase.connection
+        .update({ name: user.name, nickname: user.nickname })
+        .where({ id: user.id })
+        .into("Auth_users");
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+
+  public getUserById = async(id: string): Promise<any> => {
+    const result = await UserDatabase.connection
+      .select("*")
+      .from("Auth_users")
+      .where({ id });
+
+    return result[0];
+  }
+}
